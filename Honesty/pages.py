fd136c93503626efbe24c18ce9ec5a2bf94e1080
Sergio2405 <67673799+Sergio2405@users.noreply.github.com>
2022-01-24 15:27:13 -0500

    is displayed para tratamiento

--- Honesty/pages.py
+++ Honesty/pages.py
@@ -3,16 +3,42 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
+
+class Instructions(Page):
     pass
 
+class Time_Pressure(Page):
+
+    # form_model = 'player'
+    # form_fields = ['decision']
+
+    def is_displayed(self):
+        return self.group.treatment == 'Time Pressure'
+
+    def vars_for_template(self):
+        return dict(
+            tratamiento = self.group.treatment
+        )
+
+class Time_Delay(Page):
+    
+    def is_displayed(self):
+        return self.group.treatment == 'Time Delay'
+
+    def vars_for_template(self):
+        return dict(
+            tratamiento = self.group.treatment
+        )
+
+class Information(Page):
+    pass
 
 class ResultsWaitPage(WaitPage):
     pass
 
 
-class Results(Page):
-    pass
-
-
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [
+                Instructions,
+                Time_Pressure,
+                Time_Delay
+                ]
