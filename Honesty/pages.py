52dc38b92132ae97c80f0bf8a3033d321491b3b8
Sergio2405 <67673799+Sergio2405@users.noreply.github.com>
2022-01-24 21:19:49 -0500

    ResultsWaitPage

--- Honesty/pages.py
+++ Honesty/pages.py
@@ -15,6 +15,8 @@ class Time_Pressure(Page):
     def is_displayed(self):
         return self.group.treatment == 'Time Pressure'
 
+    timeout_seconds = 30
+
     def vars_for_template(self):
         return dict(
             grupo_asignado = self.player.grupo_asignado,
@@ -22,9 +24,14 @@ class Time_Pressure(Page):
         )
 
 class Time_Delay(Page):
+
+    form_model = 'player'
+    form_fields = ['decision']
     
     def is_displayed(self):
         return self.group.treatment == 'Time Delay'
+
+    timeout_seconds = 20    
 
     def vars_for_template(self):
         return dict(
@@ -32,15 +39,35 @@ class Time_Delay(Page):
             tratamiento = self.group.treatment
         )
 
+#tratamiento que falta codear
 class Information(Page):
     pass
 
 class ResultsWaitPage(WaitPage):
-    pass
+
+    def after_all_players_arrive(self):
+        for player in self.group.get_players():
+            player.set_comportamiento()
+
+        for player in self.group.get_players():
+            player.set_payoff()
+            
+class Payoffs(Page):
+
+    def is_displayed(self):
+        return self.player.round_number == Constants.num_rounds
+
+    def vars_for_template(self):
+
+        return dict(
+            final_payoff = self.player.participant.payoff
+        )
 
 
 page_sequence = [
                 Instructions,
                 Time_Pressure,
-                Time_Delay
+                Time_Delay,
+                ResultsWaitPage,
+                Payoffs
                 ]
