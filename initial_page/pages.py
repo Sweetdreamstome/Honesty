ae8eec006e38068acd165766ed3ec75067a20906
Sergio2405 <67673799+Sergio2405@users.noreply.github.com>
2022-02-25 08:09:27 -0500

    e2lab initial page

---
+++ initial_page/pages.py
@@ -0,0 +1,14 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+class E2lab_page(Page):
+    pass
+
+class InitialPage(Page):
+    pass
+
+
+page_sequence = [E2lab_page,
+#InitialPage
+]
